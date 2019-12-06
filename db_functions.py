78d5621c5cc6b582b117a6f7b304b34343549cc1
dima <dmitry@retailnext.net>
2019-12-06 12:32:22 +0300

    Updated database helpers

--- db_functions.py
+++ db_functions.py
@@ -4,7 +4,15 @@ import json
 database_name = 'dnd_article2'
 
 
-def add_article(*, article_name: str, article_text_ru: str, article_text_en, intent_name: str):
+def add_article(
+        *,
+        article_name: str,
+        article_text_ru: str,
+        article_text_en,
+        intent_name: str,
+        ru_header: str,
+        en_header: str
+):
     client = boto3.Session(profile_name='kreodont').client('dynamodb')
     stored_dict = client.get_item(
         TableName=database_name,
@@ -18,6 +26,8 @@ def add_article(*, article_name: str, article_text_ru: str, article_text_en, int
 
     stored_dict[intent_name]['description']['ru'] = article_text_ru
     stored_dict[intent_name]['description']['en'] = article_text_en
+    stored_dict[intent_name]['header']['en'] = en_header
+    stored_dict[intent_name]['header']['ru'] = ru_header
 
     client.put_item(TableName=database_name,
                     Item={
@@ -44,7 +54,13 @@ def get_article(*, article_name: str) -> dict:
     return stored_dict
 
 
-def update_article_attribute(*, article_name: str, new_text, attribute_name='description', language: str = 'ru', intent=None):
+def update_article_attribute(
+        *,
+        article_name: str,
+        new_text: str,
+        attribute_name: str = 'description',
+        language: str = 'ru',
+        intent: str = None):
     client = boto3.Session(profile_name='kreodont').client('dynamodb')
     stored_dict = client.get_item(
         TableName=database_name,
@@ -57,10 +73,15 @@ def update_article_attribute(*, article_name: str, new_text, attribute_name='des
         stored_dict = json.loads(stored_dict['Item']['value']['S'])
     if intent is None:
         if len(stored_dict) != 1:
-            raise Exception(f'Intent not specified, but there are more than 1 intent ({stored_dict.keys()}) for article "{article_name}"')
+            raise Exception(f'Intent not specified, but there '
+                            f'are more than 1 intent '
+                            f'({stored_dict.keys()}) for a'
+                            f'rticle "{article_name}"')
     intent = next(iter(stored_dict))
 
     new_dict = stored_dict.copy()
+    if attribute_name not in new_dict[intent]:
+        new_dict[intent][attribute_name] = {}
     new_dict[intent][attribute_name][language] = new_text
     client.put_item(TableName=database_name,
                     Item={
@@ -89,12 +110,20 @@ def update_article_attribute(*, article_name: str, new_text, attribute_name='des
 #     intent_name='condition',
 #
 # )
-print(get_article(article_name='схваченный'))
-update_article_attribute(
-    article_name='схваченный',
-    new_text='Скорость схваченного существа равна 0, и оно не получает выгоды ни от каких бонусов к скорости.  \n'
-             'Состояние оканчивается, если схвативший становится недееспособен (см. состояние).  \n'
-             'Это состояние также оканчивается, если какойлибо эффект выводит схваченное существо из зоны досягаемости '
-             'того, кто его удерживает, или из зоны удерживающего эффекта. Например, когда существо отбрасывается заклинанием волна грома.',
-)
+# print(get_article(article_name='схваченный'))
+# update_article_attribute(
+#     article_name='схваченный',
+#     new_text='- Скорость схваченного существа равна 0, и оно не получает '
+#              'выгоды ни от каких бонусов к скорости.  \n'
+#              '- Состояние оканчивается, если схвативший становится '
+#              '**недееспособен**.  \n'
+#              '- Это состояние также оканчивается, если какой-либо эффект '
+#              'выводит схваченное существо из зоны досягаемости '
+#              'того, кто его удерживает, или из зоны удерживающего эффекта. '
+#              'Например, '
+#              'когда существо отбрасывается заклинанием волна грома.',
+# )
+update_article_attribute(article_name='схваченный', intent='condition',
+                         attribute_name='title', language='ru',
+                         new_text='Схваченный')
 print(get_article(article_name='схваченный'))
